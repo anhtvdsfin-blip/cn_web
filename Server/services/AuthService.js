ac3daea7d9aed1fd6a11c1ec0a644f5ec9edac9b
Duc Anh <khamvl08@gmail.com>
2025-12-25 16:03:52 +0700

    update dockerfile

--- Server/services/AuthService.js
+++ Server/services/AuthService.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import httpStatus from 'http-status';
 import User from '../models/User.js';
-import Token from '../models/token.js';
+import Token from '../models/Token.js';
 import { buildUserResponse, normalizeGender } from './UserService.js';
 
 // HASH MẬT KHẨU
