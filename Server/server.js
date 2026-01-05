35c8fba8fffc2a94e0f6ef31ecfce820bc014e8d
Duc Anh <khamvl08@gmail.com>
2026-01-05 13:07:39 +0700

    update

--- Server/server.js
+++ Server/server.js
@@ -30,6 +30,10 @@ dotenv.config();
 
 const app = express();
 
+// Trust proxy - required when behind reverse proxy (Nginx, Cloudflare, Docker, etc.)
+// This allows express-rate-limit to correctly identify users via X-Forwarded-For header
+app.set('trust proxy', 1);
+
 // Security: set various HTTP headers
 app.use(helmet());
 
